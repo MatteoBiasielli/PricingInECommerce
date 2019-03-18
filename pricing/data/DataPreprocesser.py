f276568baf203d2606b35eee301f494b2c3ce36a
MatteoBiasielli <matteo.biasielli@mail.polimi.it>
2019-03-18 15:05:36 -0500

    bug fix

--- pricing/data/DataPreprocesser.py
+++ pricing/data/DataPreprocesser.py
@@ -9,22 +9,22 @@ PREPROCDATADIRPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '
 
 class DataPreprocesser:
 
-    def __init__(self, path=None):
+    def __init__(self, path=None, no_basic_preprocessing=False):
         rawdata = pd.read_csv(os.path.join(RAWDATADIRPATH, 'rawdata.csv') if path is None else path)
-        convs = acd.anwswers_translations()
 
-        for c in convs:
-            rawdata = rawdata.replace(to_replace=c[0], value=c[1])
-        col2col = acd.question_to_question()
-        nans = rawdata.isna()
-        for c2c in col2col:
-            for i in range(rawdata.shape[0]):
-                if nans[c2c[1]][i]:
-                    rawdata.set_value(index=i, col=c2c[1], value=rawdata[c2c[0]][i])
-        for c2c in col2col:
-            rawdata = rawdata.drop(labels=[c2c[0]], axis=1)
-        print(rawdata)
-        print(list(rawdata))
+        if not no_basic_preprocessing:
+            convs = acd.anwswers_translations()
+
+            for c in convs:
+                rawdata = rawdata.replace(to_replace=c[0], value=c[1])
+            col2col = acd.question_to_question()
+            nans = rawdata.isna()
+            for c2c in col2col:
+                for i in range(rawdata.shape[0]):
+                    if nans[c2c[1]][i]:
+                        rawdata.set_value(index=i, col=c2c[1], value=rawdata[c2c[0]][i])
+            for c2c in col2col:
+                rawdata = rawdata.drop(labels=[c2c[0]], axis=1)
         self.data = rawdata
 
     def get_processed_data(self):
@@ -35,8 +35,9 @@ class DataPreprocesser:
             self.data = foo(self.data)
 
     def save_data(self, path=None):
-        self.data.to_csv(os.path.join(PREPROCDATADIRPATH, 'processed_data.csv') if path is None else path)
+        self.data.to_csv(os.path.join(PREPROCDATADIRPATH, 'processed_data.csv') if path is None else path, index=False)
 
 
 if __name__ == '__main__':
-    DataPreprocesser().save_data()
+    # DataPreprocesser().save_data()
+    print(DataPreprocesser(path='./preprocessed_data/processed_data.csv', no_basic_preprocessing=True).data)
