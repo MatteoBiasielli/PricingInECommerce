476cdf8abee105fb1e89e832920262913614e194
cassiniste <stefano.cassini@mail.polimi.it>
2019-03-29 00:11:32 +0100

    Profit maximization in K-tesing, still not sure if correct

--- pricing/K-Testing/KTestingLearner.py
+++ pricing/K-Testing/KTestingLearner.py
@@ -7,14 +7,14 @@ from pricing.Environment import Environment
 
 
 class KTestingLearner:
-    # TODO profit maximization
-    # time horizon (exploration phase) -> n° of samples, accuracy
-    def __init__(self, environment, num_of_candidates, tot_n_of_samples, alpha=0.05):
+
+    def __init__(self, environment, num_of_candidates, marginal_profits, tot_samples, alpha=0.05):
         self.environment = environment
         self.num_of_candidates = num_of_candidates
-        self.tot_n_of_samples = tot_n_of_samples
+        self.marginal_profits = marginal_profits
+        self.tot_samples = tot_samples
         self.alpha = alpha
-        self.n_samples_per_candidate = int(tot_n_of_samples / num_of_candidates)
+        self.n_samples_per_candidate = int(tot_samples / num_of_candidates)
         # each row refers to the collected samples of one candidate
         self.samples = np.empty((num_of_candidates, self.n_samples_per_candidate), int)
         self.empirical_means = []
@@ -25,20 +25,30 @@ class KTestingLearner:
         for i in range(self.n_samples_per_candidate):
             realizations = self.environment.get_realizations()
             self.samples[:, i] = realizations
-        # compute empirical means
+
         for candidate in range(self.num_of_candidates):
-            self.empirical_means.append(np.count_nonzero(self.samples[candidate, :]) / self.n_samples_per_candidate)
+            # compute empirical means
+            mean_conversion_rate = np.count_nonzero(self.samples[candidate, :]) / self.n_samples_per_candidate
+            self.empirical_means.append(mean_conversion_rate * self.marginal_profits[candidate])
+            # turn mean conversion rate to mean profit
+            self.samples[candidate, :] = self.samples[candidate, :] * self.marginal_profits[candidate]
+
         # select the winner candidate
         winner = self.__select_winner()
-        if winner is not False:
-            print("Candidate {} is the winner!".format(winner))
-            return winner
+
+        if not winner:
+            return None
         else:
-            print("A winner could not be found, try lowering confidence and/or increasing samples")
+            return winner
 
     def __select_winner(self):
         """Selects the winning candidate if possible, otherwise returns False"""
-        # sort the candidates by empirical means
+        # normalize mean profits
+        self.empirical_means = (self.empirical_means - np.amin(self.empirical_means)) / (
+                np.amax(self.empirical_means) - np.amin(self.empirical_means))
+
+        """
+        # sort the candidates by empirical mean profits
         empirical_ranking = sorted(range(len(self.empirical_means)), key=lambda k: self.empirical_means[k],
                                    reverse=True)
         # starting from most promising candidate, test against all other candidates
@@ -46,7 +56,12 @@ class KTestingLearner:
             is_winner = self.__test_against_others(candidate)
             if is_winner:
                 return candidate
-
+        """
+        # test most promising candidate against all others
+        best_empirical = self.empirical_means.argmax()
+        is_winner = self.__test_against_others(best_empirical)
+        if is_winner:
+            return best_empirical
         # if no candidate is a winner then return False
         return False
 
@@ -82,20 +97,30 @@ class KTestingLearner:
             # cannot reject null hypothesis
             return False
 
-    def __compute_conf_interval(self, candidate):
-        """Computes the binomial confidence interval with normal approximation"""
-        p = self.empirical_means[candidate]  # sample mean
-        z_value = self.__get_z_value(1 - self.alpha)
-        lower_bound = p - (z_value * math.sqrt((p * (1 - p)) / self.n_samples_per_candidate))
-        upper_bound = p + (z_value * math.sqrt((p * (1 - p)) / self.n_samples_per_candidate))
-        return lower_bound, upper_bound
+    def __plot_reward(self, winner):
+        pass  # TODO
 
-    def __get_z_value(self, confidence):
-        """Returns the z-value at a certain confidence"""
-        return stats.norm.ppf(1 - (1 - confidence) / 2)
+    def __plot_regret(self, winner):
+        pass  # TODO
 
 
 if __name__ == '__main__':
-    env = Environment([0.7, 0.5, 0.2, 0.1])
-    learner = KTestingLearner(num_of_candidates=4, tot_n_of_samples=1000, environment=env, alpha=0.05)
-    learner.start()
+    probabilities = [0.6, 0.95, 0.85, 0.7]
+    MP = [25, 30, 35, 40]
+    print("Real values: {}".format([a * b for a, b in zip(probabilities, MP)]))
+    env = Environment(probabilities)
+    results = [0, 0, 0, 0]
+    wnf = 0
+
+    # experiments
+    for experiment in range(1000):
+        learner = KTestingLearner(num_of_candidates=4, marginal_profits=MP, tot_samples=1000,
+                                  environment=env, alpha=0.05)
+        winner_candidate = learner.start()
+        if winner_candidate is not None:
+            results[winner_candidate] += 1
+        else:
+            wnf += 1
+
+    print("Total wins by candidate: {}".format(results))
+    print("Total winner not found scenarios: {}".format(wnf))
