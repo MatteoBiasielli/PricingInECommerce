aeca0632ad31af6bd1dafd841c43e186be91b0f0
emilio <emilio.capo@mail.polimi.it>
2019-04-03 13:26:10 +0200

    Added profit maximization support for TS and SWTS | Code refactoring

--- pricing/ts/TSLearner.py
+++ pricing/ts/TSLearner.py
@@ -4,7 +4,7 @@ from collections import deque
 
 class TSLearner:
 
-    def __init__(self, n_arms, sliding_window=0, marginal_profits=0):
+    def __init__(self, n_arms, sliding_window=0, marginal_profits=[]):
         self.n_arms = n_arms
         self.sliding_window = sliding_window
         self.marginal_profits = marginal_profits
@@ -13,6 +13,7 @@ class TSLearner:
         self.reward_per_arm = [[] for i in range(n_arms)]
         self.expected_rewards = []
         self.beta_params = np.ones((n_arms, 2))
+        self.isProfitMaximization = (len(marginal_profits) > 0)
         self.isSlidingWindow = (sliding_window > 0)
 
         if self.isSlidingWindow:
@@ -21,13 +22,22 @@ class TSLearner:
     # Returns the index of the arm which corresponds to the highest sampled value from the related prior
     def get_best_arm_from_prior(self):
         thetas = np.random.beta(self.beta_params[:, 0], self.beta_params[:, 1])
+
+        if self.isProfitMaximization:
+            thetas = thetas * self.marginal_profits
+
         best_arm = np.argmax(thetas)
         return best_arm
 
     def update(self, pulled_arm, reward, exp_reward):
         self.t += 1
-        self.reward_per_arm[pulled_arm].append(reward)
-        self.expected_rewards.append(exp_reward)
+
+        if self.isProfitMaximization:
+            self.reward_per_arm[pulled_arm].append(reward*self.marginal_profits[pulled_arm])
+            self.expected_rewards.append(exp_reward*self.marginal_profits[pulled_arm])
+        else:
+            self.reward_per_arm[pulled_arm].append(reward)
+            self.expected_rewards.append(exp_reward)
 
         self.beta_params[pulled_arm][0] += reward      # alpha
         self.beta_params[pulled_arm][1] += 1 - reward  # beta
--- pricing/ts/main.py
+++ pricing/ts/main.py
@@ -5,26 +5,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-rewards_per_experiment = []
-probabilities = [0.5, 0.3, 0.7]
-ns_probabilities = [[0.5, 0.3, 0.7], [0.85, 0.3, 0.3]]
+# - PARAMETERS - #
+
+# Init parameters
+probabilities = np.array([[0.5, 0.3, 0.7], [0.5, 0.3, 0.95]]) # Structure is [] containing [] of probabilities
+marginal_profits = [10, 8, 6]
 time_horizon = 10000
-n_experiments = 1000
-marginal_profits = 0
+n_experiments = 100
 sw = 1000
 
 isNonStationary = True
+isProfitMaximization = True
 debug = False
+
+rewards_per_experiment = []
+
+# Derived parameters
+if isProfitMaximization:
+    if isNonStationary:
+        profits = probabilities * marginal_profits
+    else:
+        profits = probabilities[0] * marginal_profits
+
+
+# - PREREQUISITES CHECK - #
+
+if len(probabilities[0]) != len(marginal_profits):
+    print("ERROR: Length of arm probabilities and marginal profits do not match.")
+    exit()
+
+
+# - ALGORITHM - #
 
 for exp in range(n_experiments):
     print("Experiment " + str(exp))
 
     if isNonStationary:
-        environment = nsenv.NonStationaryEnvironment(ns_probabilities, time_horizon)
-        learner = ts.TSLearner(n_arms=len(ns_probabilities[0]), sliding_window=sw)
+        environment = nsenv.NonStationaryEnvironment(probabilities, time_horizon)
+        learner = ts.TSLearner(n_arms=len(probabilities[0]), sliding_window=sw, marginal_profits=marginal_profits)
     else:
-        environment = env.Environment(probabilities)
-        learner = ts.TSLearner(n_arms=len(probabilities))
+        environment = env.Environment(probabilities[0])
+        learner = ts.TSLearner(n_arms=len(probabilities[0]), marginal_profits=marginal_profits)
 
     realizations = environment.get_realizations()
 
@@ -38,26 +59,33 @@ for exp in range(n_experiments):
 
     rewards_per_experiment.append(learner.expected_rewards)
 
-if isNonStationary:
-    """phase_time = int(time_horizon / len(ns_probabilities))
-    clairvoyant_reward = [np.full(phase_time, np.max(ns_probabilities[i])) for i in range(len(ns_probabilities))]
-    clairvoyant_reward = np.array(clairvoyant_reward)
-    clairvoyant_reward = np.ndarray.flatten(clairvoyant_reward)
-    clairvoyant_reward = np.repeat(np.array([clairvoyant_reward]), n_experiments, axis=0)
-    """
-    clairvoyant_reward = np.full(int(time_horizon/2), 0.7)
-    clairvoyant_reward = np.concatenate((clairvoyant_reward, np.full(int(time_horizon/2), 0.85)))
 
+# - FORMATTING RESULTS - #
 
+n_phases = len(probabilities)
+phase_time = int(time_horizon / n_phases)
+
+if isProfitMaximization:
+    clairvoyant_reward = [np.full(phase_time, np.max(profits[i])) for i in range(len(probabilities))]
 else:
-    clairvoyant_reward = np.full(time_horizon, np.max(probabilities))
+    clairvoyant_reward = [np.full(phase_time, np.max(probabilities[i])) for i in range(len(probabilities))]
 
+if isNonStationary:
+    clairvoyant_reward = np.concatenate(clairvoyant_reward)
+    clairvoyant_rewards_per_experiment = np.repeat(np.array([clairvoyant_reward]), n_experiments, axis=0)
+else:
+    clairvoyant_rewards_per_experiment = np.repeat(np.array(clairvoyant_reward), n_experiments, axis=0)
+    clairvoyant_reward = np.squeeze(clairvoyant_reward)
+
+# - PLOTTING RESULTS - #
+print(clairvoyant_reward)
 plt.figure(figsize=(8, 4))
 plt.plot(np.mean(rewards_per_experiment, axis=0))
 plt.plot(clairvoyant_reward, "--k")
 plt.legend(["Avg_exp_reward", "Clairvoyant_reward"])
 plt.show()
+
 plt.figure(figsize=(8, 4))
-plt.plot(np.cumsum(np.mean(clairvoyant_reward - rewards_per_experiment, axis=0)), "r")
+plt.plot(np.cumsum(np.mean(clairvoyant_rewards_per_experiment - rewards_per_experiment, axis=0)), "r")
 plt.legend(["Avg_regret"])
 plt.show()
