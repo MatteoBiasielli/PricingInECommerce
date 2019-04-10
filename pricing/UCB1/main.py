a26b944dcc3bf57f3dbaeb87af6ea7e62a95efb6
dondoC34 <luca.dondoni@mail.polimi.it>
2019-04-10 11:57:28 +0200

    Real data injection in ucb1 code

--- pricing/UCB1/main.py
+++ pricing/UCB1/main.py
@@ -1,15 +1,24 @@
 from pricing.UCB1.UCB1_Learner import *
 from pricing.UCB1.Environment import *
+from pricing.Environment import *
 import matplotlib.pyplot as mpl
 import numpy as np
+from pricing.demand.DemandCalculator import *
 
 
 rewards_perExp = []
-probabilities = [0.3, 0.8, 0.2, 0.4]
-# NS_probabilities = [[0.15, 0.1, 0.2, 0.35], [0.35, 0.7, 0.2, 0.35], [0.5, 0.7, 0.8, 0.15]]
-timeHorizon = 10000
-numberOfExperiments = 500
-marginal_profits = [10, 5, 10, 15]
+probabilities = []
+
+all_people = DP(path='../data/preprocessed_data/processed_data.csv', no_basic_preprocessing=True)
+all_dem = DemandCalculator(all_people, smoothing_window_size=50)
+
+for i in [30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350]:
+    probabilities.append(all_dem.get_demand_at(i))
+
+NS_probabilities = [[0.15, 0.1, 0.2, 0.35], [0.35, 0.7, 0.2, 0.45], [0.5, 0.7, 0.8, 0.15]]
+timeHorizon = 18250
+numberOfExperiments = 10000
+marginal_profits = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340]
 environment = Environment(probabilities)
 
 bestCandidateExpReward = np.max(np.array(probabilities) * np.array(marginal_profits))
@@ -19,7 +28,7 @@ for i in range(numberOfExperiments):
     environment = Environment(probabilities)
 
     # The ucb1 learner, after being initialized, tries every arm once
-    learner = UCB1_Learner(num_of_arms=4, marginal_profits=marginal_profits)
+    learner = UCB1_Learner(num_of_arms=17, marginal_profits=marginal_profits)
     realizations = environment.get_realizations()
     learner.init_ucb1_algorithm(realizations)
 
@@ -39,8 +48,13 @@ clairvoyant_arm = np.zeros(timeHorizon)
 clairvoyant_arm += bestCandidateExpReward
 mpl.plot(np.mean(rewards_perExp, axis=0))
 mpl.plot(clairvoyant_arm, "--k")
-mpl.legend(["Avg_exp_reward", "Clairvoyant_reward"])
+mpl.title("Expected Reward")
+mpl.xlabel("Number Of Interactions")
+mpl.ylabel("Average_exp_reward")
+mpl.legend(["UCB1_reward", "Clairvoyant_reward"])
 mpl.show()
-mpl.plot(np.cumsum(np.mean(bestCandidateExpReward - np.array(rewards_perExp), axis=0)), "r")
-mpl.legend(["Avg_regret"])
-mpl.show()
+# mpl.plot(np.cumsum(np.mean(environment.clairvoyant_arm - np.array(rewards_perExp), axis=0)), "r")
+# mpl.legend(["Avg_regret"])
+# mpl.show()
+
+
